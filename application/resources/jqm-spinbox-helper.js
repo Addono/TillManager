e357e1e42c83c4f6fb71e56aa2d261d9a3cb0b7b
Adriaan Knapen <a.d.knapen@student.tue.nl>
2016-10-02 17:59:25 +0200

    Switched back to old jqm spinbox helper

--- application/resources/jqm-spinbox-helper.js
+++ application/resources/jqm-spinbox-helper.js
@@ -4,41 +4,24 @@
  * http://opensource.org/licenses/MIT	MIT License
  */
 $(document).ready(function() {
-    $.fn.extend({
-       changeInputVal: function (amount) {
-            $(this).val(function(i, oldval) {
-                // Get the minimum and maximum values of the input.
-                var min = parseFloat($(this).attr('min'));
-                var max = parseFloat($(this).attr('max'));
-
-                // Calculate the new value.
-                var newval = parseFloat(oldval) + amount;
-
-                // Check if the new value is within the minimum and maximum bounds.
-                if((isNaN(min) || min <= newval)
-                        && (isNaN(max) || newval <= max)) {
-                    return newval;
-                } else {
-                    return oldval;
-                }
-            });
-
-            return $(this);
-       }
-    });
-
     var counter = $('.spinbox-btn');
     var base = counter.parent().parent().children();
 
-    /*
-     * @TODO: Fix selector to be more strict.
-     */
-    $('.ui-icon-plus').click(function() {
-        $(this).parent().find(counter).changeInputVal(1);
+    base.last().click(function() {
+        $(this).parent().changeInputVal(1);
     });
 
-    $('.ui-icon-minus').click(function() {
-        $(this).parent().find(counter).changeInputVal(-1);
+    base.first().click(function(event) {
+        $(this).parent().changeInputVal(-1);
     });
 
+    $.fn.extend({
+       changeInputVal: function (amount) {
+            $(this).val(function(i, oldval) {
+                return parseFloat(oldval) + amount;
+            });
+            
+            return $(this);
+       }
+    });
 });
